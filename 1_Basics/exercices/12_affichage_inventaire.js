86b92c32661b2bc9bcc29eeb2f70aaf2f09d9b01
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 10:54:03 +0100

    exercice12

--- 1_Basics/exercices/12_affichage_inventaire.js
+++ 1_Basics/exercices/12_affichage_inventaire.js
@@ -2,3 +2,32 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const inventaire = [];
+
+const potion_soin = {
+  nom: "Potion de soin",
+  prix: 50,
+  stock: 10,
+};
+const potion_mana = {
+  nom: "Potion de mana",
+  prix: 70,
+  stock: 5,
+};
+const potion_endurance = {
+  nom: "Potion d'endurance",
+  prix: 80,
+  stock: 4,
+};
+inventaire.push(potion_soin);
+inventaire.push(potion_mana);
+inventaire.push(potion_endurance);
+
+// for(let i = 0; i < inventaire.length ; i++){
+//     console.log(inventaire[i].nom + " " +inventaire[i].prix + " " + inventaire[i].stock)
+// }
+
+for(const potion of inventaire){
+    for(const elem of Object.keys(potion)){
+        console.log(elem + " : " +potion[elem])
+    }
+}
