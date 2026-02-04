414c86004d3f960485f39d166489def7906d4ac0
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 11:50:24 +0100

    exercice16

--- 1_Basics/exercices/16_tri_simple.js
+++ 1_Basics/exercices/16_tri_simple.js
@@ -7,3 +7,21 @@ const inventaire = [
   { nom: "Potion de soin", prix: 50, stock: 10 },
   { nom: "Potion d'endurance", prix: 40, stock: 7 }
 ];
+
+for(let i = 0 ; i < inventaire.length; i++){
+  for (let j = i ; j < inventaire.length; j++){
+    if (inventaire[i].prix > inventaire[j].prix){
+      let temp = inventaire[i]
+      inventaire[i] = inventaire[j]
+      inventaire[j] = temp
+    }
+  }
+}
+
+console.log(inventaire)
+
+
+for(const potion of inventaire){
+  console.log(potion.nom + " : " + potion.prix
+  )
+}
