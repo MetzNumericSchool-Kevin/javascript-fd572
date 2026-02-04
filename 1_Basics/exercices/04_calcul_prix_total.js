3c131451b17c3e932aeadadbd117761bffce2130
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 09:21:01 +0100

    exercice4

--- 1_Basics/exercices/04_calcul_prix_total.js
+++ 1_Basics/exercices/04_calcul_prix_total.js
@@ -1,4 +1,8 @@
 // Calcul du prix total d'une commande de potion 🪙
 
 // Variables des exercices précédents (modifie selon tes valeurs)
-let prix_potion_soin = 50;
+let prix_potion_soin = 20;
+
+let nb_potion = parseInt(prompt("combien de potion ? "))
+let prix_total = nb_potion * prix_potion_soin
+console.log(`Prix de ${nb_potion} potions de soins : ${prix_total} 🪙 mon cher Aventurier. 💸`)
