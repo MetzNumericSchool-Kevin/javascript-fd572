ecfbe7a1efd6365786ba35b53370adcdc3b18373
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 11:16:19 +0100

    exercice14

--- 1_Basics/exercices/14_filtrage_selection.js
+++ 1_Basics/exercices/14_filtrage_selection.js
@@ -8,3 +8,21 @@ const inventaire = [
   { nom: "Potion de force", prix: 60, stock: 3 },
   { nom: "Potion de vitesse", prix: 35, stock: 8 }
 ];
+
+const potion_en_stock = []
+const potion_abordable = []
+
+for(const potion of inventaire){
+  if(potion.stock > 0){
+    potion_en_stock.push(potion)
+  }
+  if(potion.prix < 40){
+    potion_abordable.push(potion)
+  }
+}
+
+console.log("nb potion en stock : " + potion_en_stock.length)
+console.log("nb potion qui coutent moins de 40 : " + potion_abordable.length)
+
+console.log(potion_en_stock)
+console.log(potion_abordable)
