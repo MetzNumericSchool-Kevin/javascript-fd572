08cf41eaa3e34787983385fe6b0a7e0a089104b9
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 08:58:38 +0100

    exercice1

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,8 @@
 // Définition de variables
+
+const nomBoutique = "La boutique du magicien";
+let potionStock = 10;
+let prixPotion = 20;
+let boutiqueOuverte = true;
+console.log("la boutique est ouverte :" + boutiqueOuverte)
+console.log(nomBoutique + " vend "+ potionStock + " potions de soin à " + prixPotion)
