4947d86875d86dee25fdddb0ea6ab5c2b8bf660a
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 09:04:28 +0100

    exercice2

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -3,3 +3,9 @@
 // Variables de l'exercice précédent (modifie selon tes valeurs)
 const nom_boutique = "La Potion Enchantée";
 const boutique_ouverte = true;
+if(boutique_ouverte){
+    console.log("Bienvenue dans la boutique " + nom_boutique + " Aventurier !")
+
+}else{
+    console.log("La boutique " + nom_boutique + " est fermée, revenz plus tard Aventurier !")
+}
