b879a124985a5e5670215656c790b7ef016d3448
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 09:47:24 +0100

    exercice7

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -2,3 +2,9 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+console.log(potions[0])
+console.log(potions[potions.length-1])
+for(const potion of potions){
+	console.log("Nous avons de la " + potion)
+}
