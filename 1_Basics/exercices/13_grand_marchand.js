d47914a0ebcf7c100a1fb96d3d1c3f7f87699b22
Franck Doyen <franck.doyen57@gmail.com>
2026-02-04 11:05:04 +0100

    exercice13

--- 1_Basics/exercices/13_grand_marchand.js
+++ 1_Basics/exercices/13_grand_marchand.js
@@ -7,3 +7,29 @@ const inventaire = [
   { nom: "Potion d'endurance", prix: 40, stock: 0 },
   { nom: "Potion de force", prix: 60, stock: 3 },
 ];
+
+let potion_chere = -1
+let nom_potion_cher = ""
+
+let potion_stock = -1
+let nom_potion_stock =""
+
+let potion_sup_0 = []
+
+for(const potion of inventaire){
+  if (potion.prix > potion_chere){
+    potion_chere = potion.prix
+    nom_potion_cher = potion.nom
+  }
+    if (potion.stock > potion_stock){
+    potion_stock = potion.stock
+    nom_potion_stock = potion.nom
+  }
+  if (potion.stock > 0){
+    potion_sup_0.push(potion.nom)
+  }
+}
+
+console.log("potion la plus chere : " + nom_potion_cher)
+console.log("console avec le plus de stock : " + nom_potion_stock)
+console.log("potion avec du stock : " + potion_sup_0)
